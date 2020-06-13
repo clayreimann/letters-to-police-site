e4dca10171a4a0043553b29d89065fb41e961dae
Clay Jensen-Reimann <clayreimann@gmail.com>
2020-06-13 17:31:18 -0400

    Updates

---
+++ precache-manifest.14bb5ca370284a2a71126dd306a5b3c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cb920e2fb9275367d652d5e5e942b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58656a50e4d9f998a5d9",
+    "url": "/static/css/main.837cd78b.chunk.css"
+  },
+  {
+    "revision": "63ab0ca41a462b2d27a6",
+    "url": "/static/js/2.8e9c6ced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e9c6ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58656a50e4d9f998a5d9",
+    "url": "/static/js/main.18b90ed4.chunk.js"
+  },
+  {
+    "revision": "f2d43c665d2484a9fd6e",
+    "url": "/static/js/runtime-main.cbc7c5b6.js"
+  }
+]);
