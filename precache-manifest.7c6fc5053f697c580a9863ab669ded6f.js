ab6a4da0f8fcccbec55bbde29436ba14bc6ebe8e
Clay Jensen-Reimann <clayreimann@gmail.com>
2020-06-18 23:50:13 -0400

    Updates

---
+++ precache-manifest.7c6fc5053f697c580a9863ab669ded6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5755505d837ee90e6aea7336bc35c955",
+    "url": "/letters-to-police-site/index.html"
+  },
+  {
+    "revision": "0c98ea0aee076094c413",
+    "url": "/letters-to-police-site/static/css/main.6ebcd22e.chunk.css"
+  },
+  {
+    "revision": "63ab0ca41a462b2d27a6",
+    "url": "/letters-to-police-site/static/js/2.8e9c6ced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/letters-to-police-site/static/js/2.8e9c6ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c98ea0aee076094c413",
+    "url": "/letters-to-police-site/static/js/main.46529501.chunk.js"
+  },
+  {
+    "revision": "c4699b9e04654a4a15b1",
+    "url": "/letters-to-police-site/static/js/runtime-main.6aef9669.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.687012119301bcf55a86e716e05fe494.js"
+  "/letters-to-police-site/precache-manifest.7c6fc5053f697c580a9863ab669ded6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/letters-to-police-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
