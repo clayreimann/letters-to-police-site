d2966fc1317a5b4bc1ff40cd5fb5bab0fac0d1ef
Clay Jensen-Reimann <clayreimann@gmail.com>
2020-06-22 08:01:53 -0400

    Tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import "./Header.scss";
 import { Link, NavLink } from "react-router-dom";
+
+import "./Header.scss";
+import million from "./million.jpg";
 import { to } from "../helpers";
 
 function Header() {
@@ -8,8 +10,9 @@ function Header() {
     <header id="header" className="bg-black flex justify-center">
       <div className="container flex flex-col md:flex-row md:justify-between py-10">
         <Link to={to("/")} className="text-theme">
-          <h1 className="uppercase">
-            Million &nbsp;&nbsp;Letter &nbsp;&nbsp;Movement
+          <h1 className="uppercase wide flex justify-between items-center">
+            <img src={million} alt="Million" className="hdr-img" />
+            &nbsp;&nbsp;Letter Movement
           </h1>
         </Link>
         <ul className="flex items-center">
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -12,6 +12,8 @@ const letters = (
 function Main() {
   pageView("/");
 
+  const fbGroup = "https://www.facebook.com/groups/millionlettermovement/";
+
   return (
     <div className="page-main pt-5">
       <p className="text-2xl">
@@ -20,13 +22,13 @@ function Main() {
         peacefully respond and take direct action that will produce needed
         change.
       </p>
-      <h2>Will you help make a difference and join the Movement?</h2>
+      <h2>Will you help make a difference?</h2>
       <img src={banner} alt="Call to action banner" className="py-4" />
       <p>
         The Million Letter Movement empowers citizens to send {letters} to the
         Police Chiefs and Mayors of America’s 50 most populous, and 10 deadliest
         cities. These letters demand the{" "}
-        <a className="text-theme" href="https://8cantwait.org/">
+        <a className="ecw" href="https://8cantwait.org/">
           8 Can’t Wait
         </a>{" "}
         police reforms be adopted NOW. You will use pre-written {letters} based
@@ -35,7 +37,7 @@ function Main() {
       </p>
       <p>
         For information about the{" "}
-        <a className="text-theme" href="https://8cantwait.org/">
+        <a className="ecw" href="https://8cantwait.org/">
           8 Can’t Wait
         </a>{" "}
         policies, visit their website. These policies are proven to reduce harm
@@ -53,11 +55,8 @@ function Main() {
         </li>
         <li>
           For help customizing your letter join the{" "}
-          <a href="https://www.facebook.com/groups/millionlettermovement/">
-            Million Letter Movement Facebook Group
-          </a>{" "}
-          You will find instructions, pre-written {letters} , and more
-          information.
+          <a href={fbGroup}>Million Letter Movement Facebook Group</a> You will
+          find instructions, pre-written {letters} , and more information.
         </li>
         <li>
           Share the invitation with your friends &amp; followers, asking them to
@@ -69,13 +68,10 @@ function Main() {
           heard. Take a moment and send your letters{" "}
           <b className="text-theme">TODAY</b>.
         </li>
-        <li>
-          Join us at{" "}
-          <a href="https://www.facebook.com/groups/millionlettermovement/">
-            https://www.facebook.com/groups/millionlettermovement/
-          </a>
-        </li>
       </ul>
+      <p className="text-center text-2xl">
+        Join us at <a href={fbGroup}>{fbGroup}</a>
+      </p>
     </div>
   );
 }
